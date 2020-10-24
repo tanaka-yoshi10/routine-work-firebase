d3b406251cc9d5c37d16398e5d56e80767823144
Yoshito Tanaka <yoshito@sonicgarden.jp>
2020-10-24 13:16:13 +0900

    とりあえずfirestoreのデータを表示できるまで

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { firestore, database } from './firebase';
+import { useState, useEffect } from 'react';
 
 function App() {
-  // const roomsRef = firestore.collection('rooms');
+  const ref = firestore.collection('routines');
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    if(!ref) return;
+    const unsubscribe = ref
+      .onSnapshot(({ docs }) => {
+        // @ts-ignore
+        setItems(docs.map(_ => ({ id: _.id, ref: _.ref, ..._.data() })));
+        // docs.forEach(_ => {
+        //   console.log(docs.map(_ => ({ id: _.id, ref: _.ref, ..._.data() })))
+        // })
+      });
+    return unsubscribe;
+  }, []);
+
 
   return (
     <div className="App">
       <header className="App-header">
+        {
+          items.map((item) => {
+            // @ts-ignore
+            return (<p key={item.id}>{item.title}</p>);
+          })
+        }
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
