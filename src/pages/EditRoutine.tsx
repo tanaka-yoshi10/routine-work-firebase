d084191b7386b1add9f705855b6a162e41cba7c9
Yoshito Tanaka <yoshito@sonicgarden.jp>
2021-09-04 19:46:41 +0900

    font-size

--- src/pages/EditRoutine.tsx
+++ src/pages/EditRoutine.tsx
@@ -49,8 +49,9 @@ export const EditRoutine: React.FC = () => {
       <Heading
         mb="8"
         fontWeight="extrabold"
-        size="2x1"
+        fontSize="3xl"
         bgGradient="linear(to-r, pink.500, pink.300, blue.500)"
+        bgClip="text"
       >
         Edit Routine
       </Heading>
